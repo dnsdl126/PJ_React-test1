b3052ddce2e574084e388dd31732fc0893c5076f
dnsdl126 <dnsdl1217@naver.com>
2020-12-09 11:36:46 +0900

    redux 설정

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,17 @@
+import { combineReducers } from 'redux';
+//import user from './user_reducer';
+
+//combineReducers
+//스토어에 reducer 가 여러개가 있을수 있는데
+// 리듀서 안에서 하는일이 어떻게 state가 변하는지 보여준다음
+// 변화의 마지막값을 return해주는 것이 reducer 역할
+// state도 여러가지가 있을수 있기 때문에 reducer 도 나눠 있다
+// combineReducer을 리덕스에서 가져와서 root reducer에서 하나로
+// 합춰주도록 한다 
+// 인증이나 comment에 관한 reducer 을 만들었다고 하면
+// 이걸 하나로 묶어서 쓸수 있게한다
+const rootReducer = combineReducers({
+    //user
+})
+
+export default rootReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 원래는 createStore만 해도 되지만 
+//redux 미들웨어를 이용해서
+// promis와 function 형태의 데이터도 받을수 있기때문에
+// 그에대한 설정 
+// applyMiddleware 를 작성하면 redux에서 가지고 와진다 
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
+
+ReactDOM.render(
+    <Provider
+        store={createStoreWithMiddleware(Reducer,
+            window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+        )}
+    >
+        <App />
+    </Provider>
+    , document.getElementById('root'));
+
+
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 //document.getElementById('root') 
 // public에 있는 index.html에 있는 div id가 root 인 엘레멘트를  
